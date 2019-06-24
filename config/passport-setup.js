370721c3a5ff8ee80396b29347ebcecc31f22da7
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-06-24 11:18:26 +0530

    Added passport config

---
+++ config/passport-setup.js
@@ -0,0 +1,66 @@
+const Passport = require("passport");
+const LocalStratergy = require("passport-local").Strategy;
+const GoogleStratergy = require("passport-google-oauth2");
+const { User } = require("../mockdb");
+
+Passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+Passport.deserializeUser((id, done) => {
+  for (let i = 0; i < User.length; i++) {
+    if (User[i].id === id) {
+      done(null, User[i]);
+    }
+  }
+});
+
+Passport.use(
+  new LocalStratergy((username, password, done) => {
+    let found = false;
+    for (let i = 0; i < User.length; i++) {
+      if (User[i].username === username) {
+        found = true;
+        // Check password
+        if (User[i].password === password) {
+          done(null, User[i]);
+        } else {
+          done(null, false);
+        }
+      }
+    }
+    if (!found) {
+      done(null, false);
+    }
+  })
+);
+
+Passport.use(
+  new GoogleStratergy({
+    callbackURL: "/profile/google/redirect",
+    clientID:
+      "87837198098-ourd2c8o8k3u7ju0vr7u2juj2i7ke3m.apps.googleusercontent.com",
+    clientSecret: "ORHLub2zYeCuUIhBpFZas6UG"
+  }),
+  (accessToken, refreshToken, profile, done) => {
+    // Check if user already exists in our db
+    let found = false;
+    for (let i = 0; i < User.length; i++) {
+      if (User[i].googleId === profile.id) {
+        found = true;
+        done(null, User[i]);
+      }
+    }
+    if (found === false) {
+      // Insert into db
+      User.push({
+        username: profile.displayName,
+        googleId: profile.id
+      });
+
+      done(null, User[User.length - 1]);
+    }
+  }
+);
+
+module.exports = Passport;
--- mockdb.js
+++ mockdb.js
@@ -1,27 +1,29 @@
-let User=[
+let User = [
   {
-    id:123,
-    username:'vikram',
-    password:"test1"
+    id: 123,
+    username: "vikram",
+    password: "test1"
   },
   {
-    id:124,
-    username:'singh',
-    password:"test2"
+    id: 124,
+    username: "singh",
+    password: "test2"
   },
   {
-    id:125,
-    username:'bedi',
-    password:"test3"
+    id: 125,
+    username: "bedi",
+    password: "test3"
   },
   {
-    id:126,
-    username:'random',
-    password:"test4"
+    id: 126,
+    username: "random",
+    password: "test4"
   },
   {
-    id:127,
-    username:'user',
-    password:"test5"
+    id: 127,
+    username: "user",
+    password: "test5"
   }
-]
+];
+
+module.exports = { User };
