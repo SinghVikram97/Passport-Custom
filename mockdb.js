5a7657140ec222e17d4234c9fb09d1f640eabadd
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-06-24 11:02:45 +0530

    Added mockdb

---
+++ mockdb.js
@@ -0,0 +1,27 @@
+let User=[
+  {
+    id:123,
+    username:'vikram',
+    password:"test1"
+  },
+  {
+    id:124,
+    username:'singh',
+    password:"test2"
+  },
+  {
+    id:125,
+    username:'bedi',
+    password:"test3"
+  },
+  {
+    id:126,
+    username:'random',
+    password:"test4"
+  },
+  {
+    id:127,
+    username:'user',
+    password:"test5"
+  }
+]
